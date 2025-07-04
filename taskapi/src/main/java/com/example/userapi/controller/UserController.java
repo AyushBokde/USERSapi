bc486b0435555a3b881a5155f430e67006d5c0c5
AyushBokde <ayushbokde05@gmail.com>
2025-07-04 22:09:10 +0530

    d

---
+++ taskapi/src/main/java/com/example/userapi/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.example.userapi.controller;
+
+import java.util.Optional;
+
+import jakarta.validation.Valid;
+import com.example.userapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.userapi.Entity.User;
+import com.example.userapi.Service.UserService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+	 @Autowired
+	    private UserService userService;
+	 
+	 @Autowired
+	    private UserRepository userRepository;
+	 
+	 @GetMapping("/{id}")
+	    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+	        Optional<User> user = userRepository.findById(id);
+
+	        return user.map(ResponseEntity::ok)
+	                   .orElseGet(() -> ResponseEntity.notFound().build());
+	    }
+
+	    @PostMapping("/register")
+	    public ResponseEntity<?> registerUser(@Valid @RequestBody User user) {
+
+	        if (userService.isUsernameTaken(user.getUsername())) {
+	            return ResponseEntity.badRequest().body("Username is already taken");
+	        }
+
+	        if (userService.isEmailTaken(user.getEmail())) {
+	            return ResponseEntity.badRequest().body("Email is already registered");
+	        }
+
+	        User savedUser = userService.saveUser(user);
+
+	        return ResponseEntity.ok("User registered successfully with ID: " + savedUser.getId());
+	    }
+}
